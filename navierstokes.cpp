1e8f85e612c3803a8034ab6b1555937fdf2031ef
Jie Cheng <chengjiehust@gmail.com>
2017-11-30 20:24:32 -0500

    A minor fix in navierstokes

--- navierstokes.cpp
+++ navierstokes.cpp
@@ -197,7 +197,7 @@ namespace Fluid
         fe.component_mask(velocities));
       VectorTools::interpolate_boundary_values(dof_handler,
                                                1,
-                                               BoundaryValues<dim>(),
+                                               Functions::ZeroFunction<dim>(dim + 1),
                                                zero_constraints,
                                                fe.component_mask(velocities));
     }
