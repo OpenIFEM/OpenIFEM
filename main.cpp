054ba0c06f91ecbfe6b59509a9740401a7bc0eed
Jie Cheng <chengjiehust@gmail.com>
2017-12-05 08:42:06 -0500

    mpi linearelasticity

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "navierstokes.h"
 #include "parameters.h"
 #include "utilities.h"
+#include "mpi_linearelasticity.h"
 
 extern template class Fluid::NavierStokes<2>;
 extern template class Fluid::NavierStokes<3>;
@@ -11,55 +12,35 @@ extern template class Solid::LinearElasticSolver<3>;
 extern template class Solid::HyperelasticSolver<2>;
 extern template class Solid::HyperelasticSolver<3>;
 
-int main()
+extern template class Solid::ParallelLinearElasticity<2>;
+extern template class Solid::ParallelLinearElasticity<3>;
+
+int main(int argc, char *argv[])
 {
   using namespace dealii;
 
   try
     {
+      Utilities::MPI::MPI_InitFinalize mpi_initialization(argc, argv, 1);
+
       Parameters::AllParameters params("parameters.prm");
       if (params.dimension == 2)
         {
-          Triangulation<2> triangulation;
-          /*
+          parallel::distributed::Triangulation<2> tria(MPI_COMM_WORLD,
+            typename Triangulation<2>::MeshSmoothing
+              (Triangulation<2>::smoothing_on_refinement |
+               Triangulation<2>::smoothing_on_coarsening));
           GridGenerator::subdivided_hyper_rectangle(
-            triangulation,
+            tria,
             std::vector<unsigned int>{16, 2},
             Point<2>(0, 0),
             Point<2>(8, 1),
             true);
-          // Solid::LinearElasticSolver<2> solid(triangulation, params);
-          Solid::HyperelasticSolver<2> solid(triangulation, params);
+          Solid::ParallelLinearElasticity<2> solid(tria, params);
           solid.run();
-          */
-
-          // Utils::GridCreator::flow_around_cylinder(triangulation);
-          GridGenerator::subdivided_hyper_rectangle(
-            triangulation,
-            std::vector<unsigned int>({16U, 4U}),
-            Point<2>(),
-            Point<2>(8.0, 2.0),
-            true);
-          Fluid::NavierStokes<2> flow(triangulation, params);
-          flow.run();
         }
       else if (params.dimension == 3)
         {
-          Triangulation<3> triangulation;
-          /*
-          GridGenerator::subdivided_hyper_rectangle(
-            triangulation,
-            std::vector<unsigned int>{16, 2, 2},
-            Point<3>(0, 0, 0),
-            Point<3>(8, 1, 1),
-            true);
-          Solid::LinearElasticSolver<3> solid(triangulation, params);
-          // Solid::HyperelasticSolver<3> solid(triangulation, params);
-          solid.run();
-          */
-          Utils::GridCreator::flow_around_cylinder(triangulation);
-          Fluid::NavierStokes<3> flow(triangulation, params);
-          flow.run();
         }
       else
         {
