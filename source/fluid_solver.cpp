18e4e66a6759f20c2798f91ee3f64e30c84fd6d9
Jie Cheng <chengjiehust@gmail.com>
2018-09-22 14:39:02 -0400

    Use mapping of order fluid_velocity_degree in interpolate_boundary_values

--- source/fluid_solver.cpp
+++ source/fluid_solver.cpp
@@ -130,15 +130,18 @@ namespace Fluid
           }
         if (parameters.use_hard_coded_values == 1)
           {
-            VectorTools::interpolate_boundary_values(dof_handler,
-                                                     id,
-                                                     *boundary_values,
-                                                     nonzero_constraints,
-                                                     ComponentMask(mask));
+            VectorTools::interpolate_boundary_values(
+              MappingQGeneric<dim>(parameters.fluid_velocity_degree),
+              dof_handler,
+              id,
+              *boundary_values,
+              nonzero_constraints,
+              ComponentMask(mask));
           }
         else
           {
             VectorTools::interpolate_boundary_values(
+              MappingQGeneric<dim>(parameters.fluid_velocity_degree),
               dof_handler,
               id,
               Functions::ConstantFunction<dim>(augmented_value),
@@ -146,6 +149,7 @@ namespace Fluid
               ComponentMask(mask));
           }
         VectorTools::interpolate_boundary_values(
+          MappingQGeneric<dim>(parameters.fluid_velocity_degree),
           dof_handler,
           id,
           Functions::ZeroFunction<dim>(dim + 1),
