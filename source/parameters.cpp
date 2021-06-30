5984304065e16ab286c0b155a463241e74859201
Feimi <yufeimi.rpi@gmail.com>
2021-06-30 15:17:15 -0400

    Add S-A model BCs to parameter handler

--- source/parameters.cpp
+++ source/parameters.cpp
@@ -278,6 +278,58 @@ namespace Parameters
     prm.leave_subsection();
   }
 
+  void SpalartAllmarasModel::declareParameters(ParameterHandler &prm)
+  {
+    prm.enter_subsection("Spalart Allmaras BCs");
+    {
+      prm.declare_entry(
+        "Number of S-A model BCs",
+        "0",
+        Patterns::Integer(),
+        "Number of boundaries with Spalart-Allmaras turbulence model BCs");
+      prm.declare_entry(
+        "S-A model boundary id",
+        "",
+        Patterns::List(dealii::Patterns::Integer()),
+        "Ids of the boundaries with Spalart-Allmaras turbulence model BCs");
+      prm.declare_entry("S-A model boundary types",
+                        "",
+                        Patterns::List(dealii::Patterns::Integer(0, 1)),
+                        "Boundary condition types to specify. 0 for walls and "
+                        "1 for inflow condition");
+    }
+    prm.leave_subsection();
+  }
+
+  void SpalartAllmarasModel::parseParameters(ParameterHandler &prm)
+  {
+    prm.enter_subsection("Spalart Allmaras BCs");
+    {
+      n_spalart_allmaras_model_bcs = prm.get_integer("Number of S-A model BCs");
+      std::string raw_input = prm.get("S-A model boundary id");
+      std::vector<std::string> parsed_input =
+        Utilities::split_string_list(raw_input);
+      std::vector<int> ids = Utilities::string_to_int(parsed_input);
+      // Assert only when the user wants to impose Neumann BC
+      AssertThrow(!n_spalart_allmaras_model_bcs ||
+                    ids.size() == n_spalart_allmaras_model_bcs,
+                  ExcMessage("Inconsistent boundary ids!"));
+      raw_input = prm.get("S-A model boundary types");
+      parsed_input = Utilities::split_string_list(raw_input);
+      std::vector<double> values = Utilities::string_to_double(parsed_input);
+      // The size of values should be exactly the same as the number of
+      // the given boundary values.
+      AssertThrow(!n_spalart_allmaras_model_bcs ||
+                    values.size() == n_spalart_allmaras_model_bcs,
+                  ExcMessage("Inconsistent boundary values!"));
+      for (unsigned int i = 0; i < n_spalart_allmaras_model_bcs; ++i)
+        {
+          spalart_allmaras_model_bcs[ids[i]] = values[i];
+        }
+    }
+    prm.leave_subsection();
+  }
+
   void SolidFESystem::declareParameters(ParameterHandler &prm)
   {
     prm.enter_subsection("Solid finite element system");
@@ -548,6 +600,7 @@ namespace Parameters
     FluidSolver::declareParameters(prm);
     FluidDirichlet::declareParameters(prm);
     FluidNeumann::declareParameters(prm);
+    SpalartAllmarasModel::declareParameters(prm);
     SolidFESystem::declareParameters(prm);
     SolidMaterial::declareParameters(prm);
     SolidSolver::declareParameters(prm);
@@ -563,6 +616,7 @@ namespace Parameters
     FluidSolver::parseParameters(prm);
     FluidDirichlet::parseParameters(prm);
     FluidNeumann::parseParameters(prm);
+    SpalartAllmarasModel::parseParameters(prm);
     SolidFESystem::parseParameters(prm);
     SolidMaterial::parseParameters(prm);
     SolidSolver::parseParameters(prm);
